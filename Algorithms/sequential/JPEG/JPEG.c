6fcb9ea86717cb41a89b5edc8f7009aa4dd65503
bluesun42-hash <82324500+bluesun42-hash@users.noreply.github.com>
2025-01-10 17:58:09 +0100

    added reconstruction and some chrominance steps

--- Algorithms/sequential/JPEG/JPEG.c
+++ Algorithms/sequential/JPEG/JPEG.c
@@ -10,16 +10,19 @@
 #define IMAGES_DIRECTORY "../../../Assets/Images/"
 #define OUTPUT_DIRECTORY "../../../Output-Input/Images/"
 #define PI 3.14159265358979323846
-size_t LUMINANCE_QUANTIZATION_TABLE [64] = {
-        16, 11, 10, 16, 24, 40, 51, 61,
-        12, 12, 14, 19, 26, 58, 60, 55,
-        14, 13, 16, 24, 40, 57, 69, 56,
-        14, 17, 22, 29, 51, 87, 80, 62,
-        18, 22, 37, 56, 68, 109, 103, 77,
-        24, 35, 55, 64, 81, 104, 113, 92,
-        49, 64, 78, 87, 103, 121, 120, 101,
-        72, 92, 95, 98, 112, 100, 103, 99
-    };
+size_t LUMINANCE_QUANTIZATION_TABLE[64] = {
+    8,   6,   6,   8,   10,  14,  18,  22,
+    6,   6,   7,   9,   12,  20,  22,  20,
+    6,   7,   8,   10,  14,  22,  25,  22,
+    8,   9,   10,  14,  18,  28,  27,  22,
+    10,  12,  14,  18,  22,  35,  33,  26,
+    14,  18,  22,  22,  27,  33,  36,  30,
+    18,  22,  26,  28,  33,  40,  40,  34,
+    22,  26,  28,  30,  36,  34,  35,  33
+};
+
+
+
 
 size_t CHROMINANCE_QUANTIZATION_TABLE [64] = {
 17, 18, 24, 47, 99, 99, 99, 99,
@@ -45,9 +48,14 @@ typedef struct {
 } ImageData;
 
 typedef struct {
-    uint8_t values[64];
+    uint8_t lum_values[64];
+    uint8_t b_values[32];
+    uint8_t r_values[32];
+
     size_t index;
-    double* coefficients;
+    double* lum_coefficients;
+    double* r_coefficients;
+    double* b_coefficients;
 } PixelGroup;
 
 void free_pixels(Pixel** pixels, int height) {
@@ -300,6 +308,28 @@ void chroma_subsample(uint8_t*** values, ImageData data) {
     free(sub_sample);
 }
 
+
+
+double calculate_mse(uint8_t** original, Pixel** reconstructed, size_t rows, size_t cols) {
+    double mse = 0.0;
+
+    // Loop over all the pixels in the image (rows x cols)
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            double diff = (double)original[i][j] - reconstructed[i][j].r;  // Get difference for each pixel
+            mse += diff * diff;  // Sum squared differences
+        }
+    }
+
+    mse /= (rows * cols);  // Average the squared differences
+
+    // Print the MSE
+    printf("Mean Squared Error (MSE): %f\n", mse);
+
+    return mse;
+}
+
+
 void inverse_discrete_cosine_transform(uint8_t (*values)[64], size_t data_length, double* coefficients) {
 
     // Temporary array to store calculated values
@@ -362,8 +392,7 @@ void discrete_cosine_transform(uint8_t* data, size_t data_length, double** coeff
     free(corrected_values);
 }
 
-PixelGroup* divide_image(uint8_t** values, ImageData data, size_t group_size) {
-
+PixelGroup* divide_image(uint8_t** luminance_values, uint8_t** rChrominance_values, uint8_t** bChrominance_values, ImageData data, size_t group_size) {
     size_t blocks_per_row = (data.width + group_size - 1) / group_size;  
     size_t blocks_per_col = (data.height + group_size - 1) / group_size; 
     size_t group_count = blocks_per_row * blocks_per_col;
@@ -377,14 +406,16 @@ PixelGroup* divide_image(uint8_t** values, ImageData data, size_t group_size) {
         return NULL;
     }
 
-
     for (size_t i = 0; i < group_count; i++) {
         for (size_t j = 0; j < group_size * group_size; j++) {
-            groups[i].values[j] = 0; 
+            groups[i].lum_values[j] = 0; // Initialize luminance
+        }
+        for (size_t j = 0; j < group_size * (group_size / 2); j++) {
+            groups[i].r_values[j] = 0; // Initialize R chrominance
+            groups[i].b_values[j] = 0; // Initialize B chrominance
         }
     }
 
-   
     for (size_t row = 0; row < data.height; row++) {
         for (size_t col = 0; col < data.width; col++) {
             size_t block_row = row / group_size;
@@ -395,17 +426,61 @@ PixelGroup* divide_image(uint8_t** values, ImageData data, size_t group_size) {
             size_t local_col = col % group_size;
             size_t local_index = local_row * group_size + local_col;
 
-            groups[block_index].values[local_index] = values[row][col];
+            groups[block_index].lum_values[local_index] = luminance_values[row][col];
+
+            // For 4:2:2 chrominance subsampling
+            if (local_col % 2 == 0) { // Process chrominance for even columns
+                size_t chroma_index = local_row * (group_size / 2) + (local_col / 2);
+                groups[block_index].r_values[chroma_index] = rChrominance_values[row][col / 2];
+                groups[block_index].b_values[chroma_index] = bChrominance_values[row][col / 2];
+            }
         }
     }
-
-
 
     return groups;
 }
 
-void assemble_image(ImageData* output, ImageData original, PixelGroup* groups){
-    //stitch together the blocks
+
+void assemble_image(ImageData* output, ImageData original, PixelGroup* groups) {
+
+    output->height = original.height;
+    output->width = original.width;
+    output->pixel_count = original.pixel_count;
+
+    output->pixels = malloc(output->height * sizeof(Pixel*));
+    for (int y = 0; y < output->height; y++) {
+        output->pixels[y] = malloc(output->width * sizeof(Pixel));
+    }
+
+
+    const size_t group_size = 8; 
+    const size_t blocks_per_row = (original.width + group_size - 1) / group_size;
+    const size_t blocks_per_col = (original.height + group_size - 1) / group_size;
+
+
+    for (size_t block_row = 0; block_row < blocks_per_col; block_row++) {
+        for (size_t block_col = 0; block_col < blocks_per_row; block_col++) {
+            size_t block_index = block_row * blocks_per_row + block_col;
+
+            for (size_t local_row = 0; local_row < group_size; local_row++) {
+                for (size_t local_col = 0; local_col < group_size; local_col++) {
+                    size_t global_row = block_row * group_size + local_row;
+                    size_t global_col = block_col * group_size + local_col;
+
+
+                    if (global_row < output->height && global_col < output->width) {
+                        size_t local_index = local_row * group_size + local_col;
+                        uint8_t luminance = groups[block_index].lum_values[local_index];
+
+                        output->pixels[global_row][global_col].r = luminance;
+                        output->pixels[global_row][global_col].g = luminance;
+                        output->pixels[global_row][global_col].b = luminance;
+                        output->pixels[global_row][global_col].a = 255; 
+                    }
+                }
+            }
+        }
+    }
 }
 
 void Quantize(double** luminance){
@@ -424,73 +499,130 @@ void Inverse_quantize(double** luminance){
 }
 
 
+
 int main() {
+    printf("Constructing file path...\n");
     char* path = construct_path("og.png", IMAGES_DIRECTORY);
     ImageData image = read_image(path);
     free(path);
-
+    
+    printf("Creating initial PNG image from input data...\n");
     create_png_image("original.png", image.width, image.height, image.pixels);
 
-
+    printf("Building luminance matrix...\n");
     uint8_t** luminance_matrix;
     build_luminance_matrix(image, &luminance_matrix);
+
+    printf("Building red chrominance matrix...\n");
     uint8_t** rChrominance_matrix;
     build_rChrominance_matrix(image, &rChrominance_matrix);
+
+    printf("Performing chroma subsampling on red chrominance...\n");
     chroma_subsample(&rChrominance_matrix, image);
 
-
+    printf("Building blue chrominance matrix...\n");
     uint8_t** bChrominance_matrix;
     build_bChrominance_matrix(image, &bChrominance_matrix);
+
+    printf("Performing chroma subsampling on blue chrominance...\n");
     chroma_subsample(&bChrominance_matrix, image);
 
+    size_t total_blocks = (size_t)ceil((double)image.pixel_count / 64);
+    printf("Dividing input into 8x8 blocks (Total blocks: %zu)...\n", total_blocks);
+    PixelGroup* blocks = divide_image(luminance_matrix, rChrominance_matrix, bChrominance_matrix, image, 8);
 
-
-     size_t total_blocks = (size_t)ceil((double)image.pixel_count / 64);
-         printf("dividing input into 8x8 blocks...\n");
-     PixelGroup* blocks = divide_image(luminance_matrix, image, 8);
-      printf("First group (8x8 block):\n");
-    for (size_t i = 0; i < 8; i++) { 
+    printf("First luminance block (8x8):\n");
+    for (size_t i = 0; i < 8; i++) {
         for (size_t j = 0; j < 8; j++) {
-            printf("%d ", (int)(blocks[0].values[i * 8 + j])); 
-        }
-        printf("\n");
-    }
-     printf("Computing DCT-II coefficients...\n");
-     for(size_t i = 0; i < total_blocks; i++){
-        
-         discrete_cosine_transform(blocks[i].values, 64, &(blocks[i].coefficients));;
-     }
-     printf("Quantizing the matrices...\n");
-     for(size_t i = 0; i < total_blocks; i++){
-        
-         Quantize(&(blocks[i].coefficients));
-
-     }
-     printf("Inverse-quantizing the matrices...\n");
-     for(size_t i = 0; i < total_blocks; i++){
-         Inverse_quantize(&(blocks[i].coefficients));
-     }
-
-         printf("Inverse-DCT the values...\n");
-     for(size_t i = 0; i < total_blocks; i++){
-         inverse_discrete_cosine_transform(&(blocks[i].values), 64, blocks[i].coefficients);
-     }
-
- printf("First group (8x8 block):\n");
-    for (size_t i = 0; i < 8; i++) { 
-        for (size_t j = 0; j < 8; j++) {
-            printf("%d ", (int)(blocks[0].values[i * 8 + j])); 
+            printf("%d ", (int)(blocks[100].lum_values[i * 8 + j]));
         }
         printf("\n");
     }
 
-    
-    for(size_t i = 0; i < total_blocks; i++){
-         free(blocks[i].coefficients);
+
+
+    printf("First red chrominance block (4x8):\n");
+    for (size_t i = 0; i < 4; i++) {
+        for (size_t j = 0; j < 8; j++) {
+            printf("%d ", (int)(blocks[0].r_values[i * 8 + j]));
+        }
+        printf("\n");
     }
-     free(blocks);
+
+
+
+    printf("Computing DCT-II coefficients for all blocks...\n");
+    for (size_t i = 0; i < total_blocks; i++) {
+       // printf("Processing block %zu/%zu\n", i + 1, total_blocks);
+        discrete_cosine_transform(blocks[i].lum_values, 64, &(blocks[i].lum_coefficients));
+        discrete_cosine_transform(blocks[i].r_values, 32, &(blocks[i].r_coefficients));
+        discrete_cosine_transform(blocks[i].b_values, 32, &(blocks[i].b_coefficients));
+    }
+
+        printf("First luminance coefficients block (8x8):\n");
+    for (size_t i = 0; i < 8; i++) {
+        for (size_t j = 0; j < 8; j++) {
+            printf("%d ", (int)(blocks[100].lum_coefficients[i * 8 + j]));
+        }
+        printf("\n");
+    }
+
+
+
+    printf("First red coefficients block (4x8):\n");
+    for (size_t i = 0; i < 4; i++) {
+        for (size_t j = 0; j < 8; j++) {
+            printf("%d ", (int)(blocks[0].r_coefficients[i * 8 + j]));
+        }
+        printf("\n");
+    }
+
+    printf("Quantizing the matrices for all blocks...\n");
+    for (size_t i = 0; i < total_blocks; i++) {
+        //printf("Quantizing block %zu/%zu\n", i + 1, total_blocks);
+        Quantize(&(blocks[i].lum_coefficients));
+    }
+
+    printf("Inverse-quantizing the matrices for all blocks...\n");
+    for (size_t i = 0; i < total_blocks; i++) {
+        //printf("Inverse quantizing block %zu/%zu\n", i + 1, total_blocks);
+        Inverse_quantize(&(blocks[i].lum_coefficients));
+    }
+
+    printf("Performing Inverse-DCT on the values for all blocks...\n");
+    for (size_t i = 0; i < total_blocks; i++) {
+       // printf("Inverse DCT on block %zu/%zu\n", i + 1, total_blocks);
+        inverse_discrete_cosine_transform(&(blocks[i].lum_values), 64, blocks[i].lum_coefficients);
+    }
+
+    printf("Assembling the reconstructed image...\n");
+    ImageData new_image = {0};
+    assemble_image(&new_image, image, blocks);
+
+    printf("Saving the reconstructed PNG image...\n");
+    create_png_image("reconstructed.png", new_image.width, new_image.height, new_image.pixels);
+
+    printf("Calculating Mean Squared Error (MSE)...\n");
+    calculate_mse(luminance_matrix, new_image.pixels, new_image.height, new_image.width);
+
+    printf("First luminance block after reconstruction (8x8):\n");
+    for (size_t i = 0; i < 8; i++) {
+        for (size_t j = 0; j < 8; j++) {
+            printf("%d ", (int)(blocks[100].lum_values[i * 8 + j]));
+        }
+        printf("\n");
+    }
+
+    printf("Freeing allocated memory...\n");
+    for (size_t i = 0; i < total_blocks; i++) {
+        free(blocks[i].lum_coefficients);
+        free(blocks[i].b_coefficients);
+        free(blocks[i].r_coefficients);
+    }
+    free(blocks);
     free_pixels(image.pixels, image.height);
-    
+    free_pixels(new_image.pixels, new_image.height);
 
+    printf("Program completed successfully.\n");
     return 0;
 }
